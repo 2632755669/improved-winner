c3bf70e809462b8ab2b6e38a1cea2814806a9032
wb_xiaomingdeng <wb_xiaomingdeng@meituan.com>
2022-12-16 14:18:12 +0800

    修改面包屑

--- src/pages/Detail/components/Breadcrumb/index.tsx
+++ src/pages/Detail/components/Breadcrumb/index.tsx
@@ -1,3 +1,25 @@
+import { Link } from 'react-router-dom';
+import classnames from 'classnames';
+import './index.less';
+
 export const Breadcrumb = () => {
-  return <div>面包屑</div>;
+  return (
+    <div className="text-white-42 text-base">
+      <Link to="/home">
+        <span className="breadcrumb-item cursor-pointer">首页</span>
+      </Link>
+      <span className="breadcrumb-icon">/</span>
+      <Link to="/home">
+        <span className="breadcrumb-item cursor-pointer">热门服务</span>
+      </Link>
+      <span className="breadcrumb-icon">/</span>
+      <span
+        className={`${classnames({
+          'text-white-84': true,
+        })} breadcrumb-border cursor-pointer`}
+      >
+        介绍行业发展情况
+      </span>
+    </div>
+  );
 };
