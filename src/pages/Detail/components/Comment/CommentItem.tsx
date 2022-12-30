6bb63d5916fe6a5c7a64d878d23079f7314e3f11
wb_xiaomingdeng <wb_xiaomingdeng@meituan.com>
2022-12-30 20:07:53 +0800

    修改评论提示间距

--- src/pages/Detail/components/Comment/CommentItem.tsx
+++ src/pages/Detail/components/Comment/CommentItem.tsx
@@ -27,33 +27,35 @@ export const CommentItem = (props: Props) => {
   const { data, onDelete, className, disPlayTip } = props;
 
   return (
-    <section
-      className={`flex text-white-84 text-base py-6 ${classnames({
-        'border-bottom-gray': !disPlayTip,
-      })} comment-item-container ${className || ''}`}
-    >
-      <img
-        src={data.avatar || defaultAvatar}
-        alt="user-avatar"
-        className="w-28px h-28px rounded-full object-cover img-border"
-      />
-      <section className="ml-2">
-        <div>
-          <span>{data.name}</span>
-          <span className="text-white-42 text-sm ml-2">{data.creatTime}</span>
-          {data.label === labelEnum.NON_FEATURED ? (
-            <span className="comment-item-label">未精选</span>
+    <>
+      <section
+        className={`flex text-white-84 text-base py-6 ${classnames({
+          'border-bottom-gray': !disPlayTip,
+        })} comment-item-container ${className || ''}`}
+      >
+        <img
+          src={data.avatar || defaultAvatar}
+          alt="user-avatar"
+          className="w-28px h-28px rounded-full object-cover img-border"
+        />
+        <section className="ml-2">
+          <div>
+            <span>{data.name}</span>
+            <span className="text-white-42 text-sm ml-2">{data.creatTime}</span>
+            {data.label === labelEnum.NON_FEATURED ? (
+              <span className="comment-item-label">未精选</span>
+            ) : null}
+          </div>
+          <p className="mt-1.5 leading-snug">{data.content}</p>
+          {data.isMyComment ? (
+            <span
+              className="mt-1.5 text-white-42 text-sm cursor-pointer"
+              onClick={() => onDelete?.()}
+            >
+              删除
+            </span>
           ) : null}
-        </div>
-        <p className="mt-1.5 leading-snug">{data.content}</p>
-        {data.isMyComment ? (
-          <span
-            className="mt-1.5 text-white-42 text-sm cursor-pointer"
-            onClick={() => onDelete?.()}
-          >
-            删除
-          </span>
-        ) : null}
+        </section>
       </section>
       {disPlayTip ? (
         <div className="comment-mid-lines">
@@ -64,6 +66,6 @@ export const CommentItem = (props: Props) => {
           <span className="comment-mid-line" />
         </div>
       ) : null}
-    </section>
+    </>
   );
 };
